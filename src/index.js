66210e288338548dbda2004ccecfc972042088b6
Matheus Leite <matheusjoselfm@gmail.com>
2020-04-29 15:20:07 -0300

    Estrutura inicial do projeto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View style={{ width: '100%', alignItems: 'center', alignSelf: 'stretch' }}>
+      <Text style={{ fontSize: 30, textAlign: 'center' }}>Hello World</Text>
+    </View>
+  );
+}
